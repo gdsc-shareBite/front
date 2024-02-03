bb6c2c159cb69a93e1708e2f593baa95e7d5a8c9
xoxun <00xxs2@naver.com>
2024-02-04 03:09:06 +0900

    수정

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -4,18 +4,24 @@ import styled from "styled-components";
 const HistoryBox = styled.div`
   width: 75vw;
   height: 110px;
-  background-color: #dedede;
+  background-color: #ffffff;
   border-radius: 30px;
   margin: 10px 0;
   display: flex;
   justify-content:start;
   align-items: center;
+  box-shadow: 1px 3px 3px 3px #ececec;
 `;
 const Contents = styled.div`
   display:flex;
   flex-direction: column;
   
   margin: 10px 20px;
+`;
+const HistoryDate = styled.span`
+font-size: 12px;
+color: #5d5d5d;
+margin-bottom: 8px;
 `;
 const HistoryTitle = styled.span`
   font-size: 16px;
@@ -30,28 +36,48 @@ const HistoryImg = styled.img`
   margin-left: 20px;
 `;
 
+const BtnBox = styled.div`
+width: 300px;
+`;
 const Btn = styled.button`
+  width: 100px;
   height: 30px;
-  background-color: #ffffff;
+  margin: 0 10px 0 0;
+  font-weight: 400;
+  background-color: #f0f0f0;
   border: 0px solid white;
-  border-radius: 20px;
+  border-radius: 10px;
+  box-shadow: 1px 2px 3px 2px #ebebeb;
   transition: background-color 0.2s ease-in ;
   &:hover{
-    background-color: #f1f1f1;
+    background-color: #dedede;
   }
+  
 `;
 
 const AcceptBtn = styled(Btn)``;
-const RejectBtn = styled(Btn)``;
+const RejectBtn = styled(Btn)`
+  color: white;
+  background-color: #555657;
+  border: 0px solid white;
+  &:hover{
+    background-color: #777777;
+  }
+`;
 
 
-function History({postTitle="title"}) {
+function History({historyDate="1/11",postTitle="title"}) {
   return <>
     <HistoryBox>
         <HistoryImg src="" alt="-"/>
         <Contents>
+          <HistoryDate>{historyDate}</HistoryDate>
           <HistoryTitle>{postTitle}</HistoryTitle>
-          <AcceptBtn>Accept</AcceptBtn>
+          <BtnBox>
+            <AcceptBtn>Accept</AcceptBtn>
+            <RejectBtn>Reject</RejectBtn>
+          </BtnBox>
+          
         </Contents>
        
     </HistoryBox>
--- src/pages/Usage-history.jsx
+++ src/pages/Usage-history.jsx
@@ -67,14 +67,19 @@ const Hr = styled.hr`
   width: 90vw;
 `;
 const Title = styled.h1`
+  font-size: 26px;
+  font-weight: 600;
+  margin-top: 20px;
+  margin-left: 60px;
 `;
 const Subtitle = styled.h2`
   font-size: 16px;
-  margin-left: 10px;
+  font-weight: 600;
+  margin: 10px 0 0 15px;
 `;
 const Container = styled.div`
-  margin: 10px 50px;
-  background-color:#f8f8f8;
+  margin: 20px 50px 0 50px;
+  background-color:#f3f3f3;
   border-radius: 30px;
   
   display: flex;
@@ -94,30 +99,65 @@ const ReservationBox = styled.div`
 const ProgressBox = styled(ReservationBox)``;
 const CompletionBox = styled(ReservationBox)``;
 
+
+
+
 function UsageHistory() {
+
+  const data = [
+    {
+      position: 1,
+      title:"this is title",
+      imgSrc:"--",
+      date:"1/1",
+    },{
+      position: 1,
+      title:"hi",
+      imgSrc:"--",
+      date:"1/2",
+    },{
+      position: 2,
+      title:"빵",
+      imgSrc:"--",
+      date:"1/3",
+    },{
+      position: 3,
+      title:"케이크",
+      imgSrc:"--",
+      date:"1/4",
+    },{
+      position: 3,
+      title:"도넛",
+      imgSrc:"--",
+      date:"1/5",
+    },
+  ];
   return <>
-  
+  <GlobalStyle />
     <Wrapper>
       <Title>History</Title>
-      <Hr />
+      
       <Container>
         <Subtitle>예약 신청</Subtitle>
         <ReservationBox>
-          <History postTitle={""}/>
-          <History />
+        {data.map((history)=> history.position === 1 ?
+          <History postTitle={history.title} historyDate={history.date}/> : null)}
         </ReservationBox>
         <Hr />
+
         <Subtitle>진행 중</Subtitle>
         <ProgressBox>
           
-          <History />
+        {data.map((history)=> history.position === 2 ?
+          <History postTitle={history.title} historyDate={history.date}/> : null)}
         </ProgressBox>
         <Hr />
 
-        <Subtitle>예약 신청</Subtitle>
+        <Subtitle>거래 완료</Subtitle>
         <CompletionBox>
-          <History />
-          <History />
+          
+        {data.map((history)=> history.position === 3 ?
+          <History postTitle={history.title} historyDate={history.date}/> : null)}
         </CompletionBox>
         
       </Container>
