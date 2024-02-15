d68dbf6b8744e8ee543c819e9b6521144bb838cc
xoxun <00xxs2@naver.com>
2024-02-15 12:55:36 +0900

    테마 주석 추가

--- src/theme.js
+++ src/theme.js
@@ -2,8 +2,8 @@ const theme= {
   green: "#859F71",
   LightGreen: "#BCCBA4",
   darkGreen: "#596E5A",
-  darkGray: "#292929",
-  lightGray: "#f0f0f0",
+  darkGray: "#292929", // 기본 text color
+  lightGray: "#f0f0f0", //초록색 버튼 text color
 }
 
 export default theme;
