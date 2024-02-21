c566c5341379870bc954591ab3b3d50d69e447b9
JeongminSeon <46957316+JeongminSeon@users.noreply.github.com>
2024-02-22 03:47:02 +0900

    이미지 갤러리 추가

---
+++ src/components/Modal/ReservationModal.jsx
@@ -0,0 +1,59 @@
+import React, { forwardRef, useRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
+
+import styled from "styled-components";
+
+const ReservationModal = forwardRef(function ReservationModal({ setIsReserved }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => ({
+    open() {
+      dialog.current.showModal();
+    },
+    close() {
+      setIsReserved(true);
+      dialog.current.close();
+    },
+  }));
+
+  return createPortal(
+    <ModalContainer ref={dialog}>
+      <p>예약 성공!</p>
+      <form method="dialog">
+        <Button onClick={() => ref.current.close()}>Close</Button>
+      </form>
+    </ModalContainer>,
+    document.getElementById("modal")
+  );
+});
+
+export default ReservationModal;
+
+const ModalContainer = styled.dialog`
+  border: none;
+  border-radius: 8px;
+  padding: 2rem;
+  min-width: fit-content;
+  width: 30%;
+  &::backdrop {
+    background: rgba(0, 0, 0, 0.5);
+  }
+  p {
+    margin: 0.5rem 0;
+    font-size: 1.2remm;
+  }
+  form {
+    text-align: right;
+  }
+`;
+
+const Button = styled.button`
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 4px;
+  font-size: 1.2rem;
+  color: white;
+  background: #35314c;
+  cursor: pointer;
+`;
--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,12 +1,106 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useContext, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import { PostContext } from "../store/PostContext";
+import ImageGallery from "react-image-gallery";
+import 'react-image-gallery/styles/css/image-gallery.css';
+import styled from "styled-components";
+import ReservationModal from "../components/Modal/ReservationModal";
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
 
 export default function PostDetail() {
-  const {
-    state: {
-      product: { id, photo, rating, name, location, author },
-    },
-  } = useLocation();
+  const [isReserved, setIsReserved] = useState(false);
 
-  return <div>상세 페이지</div>;
+  const {id} = useParams();
+  const { data : posts} = useContext(PostContext);
+  const post = posts.find(p => p.id.toString() === id);
+  
+  const dialog = useRef();
+  function openReservationModal() {
+    dialog.current.open();
+  }
+
+
+  return (
+    <>
+      {post ? (
+        <>
+        <ReservationModal ref={dialog} setIsReserved={setIsReserved} />
+        <Div>
+          <ImageGallery 
+            items={images} 
+            showPlayButton={false}
+            showFullscreenButton={false}
+            showIndex={true}
+            style={{marginRight : "30px"}}
+          />
+          <Detail>
+            <Title>{post.title}</Title>
+            <Description>{post.description}</Description>
+            {
+              isReserved ?  <Button onClick={setIsReserved(false)}>예약취소</Button> 
+              : <Button onClick={openReservationModal}>예약하기</Button>
+            }
+            
+          </Detail>
+        </Div>
+        </>
+      ) : (
+        <p>포스트를 찾을 수 없습니다.</p>
+      )}
+    </>
+  );
 }
+
+const Div = styled.div`
+  display : flex;
+  justify-content: center;
+  padding: 50px 30px 50px;
+`
+
+const Detail = styled.div`
+  margin-left : 40px;  
+  width : 600px;
+  border-radius: 8px;
+  background: #f4f4f4;
+  padding: 20px;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+`
+
+const Title = styled.h1`
+  color: #333;
+  font-size: 24px;
+`;
+
+const Description = styled.p`
+  color: #666;
+  line-height: 1.6;
+`;
+
+const Button = styled.button`
+  background-color: purple;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 10px;
+
+  &:hover {
+    background-color: #C11B33;
+  }
+`;
